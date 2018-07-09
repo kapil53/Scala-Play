3e5e2b787ad2af8f9539654ce5b14a16288be61a
kapil sanghvi <kapil.sanghvi@anatwine.com>
2018-07-09 16:01:20 +0100

    Updated App implementation with models and views added

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,32 +5,31 @@ import play.api._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
-  def listProducts() = Action { implicit request: Request[AnyContent] =>
-    //service.findAll()
-    //Ok(views.html.productsList())
-    Ok("To be Implemented")
+  def productListing() = Action {implicit request: Request[AnyContent] =>
+    val productList = models.ProductService.findAll()
+    Ok(views.html.productListing(productList))
   }
 
-  def listSpecificProducts(count: String) = Action { implicit request: Request[AnyContent] =>
-    //service.findAll()
-    //Ok(views.html.productsList())
-    Ok("To be Implemented"+ count)
+  def productSpecificListing(count: String) = Action {implicit request: Request[AnyContent] =>
+    val productList = models.ProductService.findAll()
+    Ok(views.html.productListing(productList))
+    Ok(s"To be implemented $count")
   }
 }
